ef1a7b89585fd5672b412043225271f946a7f592
quinmccl <herobrinemcq@gmail.com>
2021-12-03 08:05:36 -0500

    changed collision step to be n dimention tollerant

--- physics.c
+++ physics.c
@@ -1,6 +1,35 @@
 #include "physics.h"
 #include <math.h>
 //should move by one physics step
+
+float vec_dot(vec_t a, vec_t b)
+{
+    float product_sum = 0.0f;
+    for(int d = 0; d < dims; d++)
+    {
+        product_sum += a.v[d] * b.v[d];
+    }
+    return product_sum;
+}
+vec_t vec_sub(vec_t a, vec_t b)
+{
+    vec_t diff;
+    for(int d = 0; d < dims; d++)
+    {
+        diff.v[d] = a.v[d] - b.v[d];
+    }
+    return diff;
+}
+vec_t vec_add(vec_t a, vec_t b)
+{
+    vec_t sum;
+    for(int d = 0; d < dims; d++)
+    {
+        sum.v[d] = a.v[d] + b.v[d];
+    }
+    return sum;
+}
+
 void physics_step(Particle * particle_list, int numParticles, Box * Boxes, float timestep)
 {
     while(timestep > 0)
@@ -14,22 +43,38 @@ void physics_step(Particle * particle_list, int numParticles, Box * Boxes, float
 
         for (int i = 0; i < numParticles; i++) {
             int pnum = 0;
-            vec_t dest;
-            dest = particle_list[i].pos;
-            dest[0] += particle_list[i].vel.v[0];
-            dest[1] += particle_list[i].vel.v[1];
-            int* colls = get_within_bounds(Boxes, 0, particle_list[i].pos, dest, *pnum);
+            vec_t min;
+            vec_t max;
+            for(int j = 0; j < dims; j++)
+		    {
+			    max.v[j] = fmax(particle_list[i].pos.v[j], particle_list[i].pos.v[j] + (particle_list[i].vel.v[j]*timestep)) + particle_list[i].radius;
+			    min.v[j] = fmin(particle_list[i].pos.v[j], particle_list[i].pos.v[j] + (particle_list[i].vel.v[j]*timestep)) - particle_list[i].radius;
+		    }
+            int* colls = get_within_bounds(Boxes, 0, min, max, &pnum);
             if (colls != NULL) {
                 for (int j = 0; j < pnum; j++) {
-                    float tempdist = (float)sqrt(((pow((double)(particle_list[colls[0]].pos.v[0] - particle_list[i].pos.v[0]), 2.0)) + (pow((double)(particle_list[colls[0]].pos.v[1] - particle_list[i].pos.v[1]), 2.0))));
-                    float vel = (float)sqrt(pow((double)particle_list[i].vel.v[0], 2) + pow((double)particle_list[i].vel.v[1], 2));
-                    tempcoll = tempdist / vel;
-                    if (tempcoll < collision_step) {
-                        dist = tempdist;
+                    if (colls[j] >= i) continue;
+                    
+                    float squareR = pow((particle_list[i].radius + particle_list[colls[j]].radius),2); // Sum of particle radius' squared
+                    vec_t posdiff = vec_sub(particle_list[i].pos, particle_list[colls[j]].pos); // vector difference in particle positions
+                    vec_t veldiff = vec_sub(particle_list[i].vel, particle_list[colls[j]].vel); // vector difference in particle velocities
+                    
+                    float pos_dot = vec_dot(posdiff,posdiff);
+                    float pos_vel_dot = vec_dot(posdiff,veldiff);
+                    float vel_dot = vec_dot(veldiff,veldiff);
+                    
+                    float t = pow((pos_vel_dot/2.0),2.0) - (vel_dot * (pos_dot - squareR));
+                    
+                    if (t < 0)continue;
+                    
+                    t -=  pos_vel_dot;
+                    t /= vel_dot;
+                    if (t < collision_step) {
                         index[0] = i;
-                        index[1] = colls[0];
-                        collision_step = tempcoll;
+                        index[1] = colls[j];
+                        collision_step = t;
                     }
+                    
                 }
             }
             
